606853f713e23678bf486e1f6bf2f5f0edc7ee2a
Jacpio <jacpio512383881@gmail.com>
2024-07-30 13:41:33 +0200

    First Episode
    PL: Wygenerowany projekt z podstawowymi funkcjami. Lekko zmodyfikowana konfiguracja
    
    ENG: Generated project with basic functions. Slightly modified configuration

---
+++ core/src/main/java/pl/jacpio/Launcher.java
@@ -0,0 +1,36 @@
+package pl.jacpio;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Launcher extends Game {
+    private SpriteBatch batch;
+    private Texture texture;
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        texture = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(texture, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+    }
+}
